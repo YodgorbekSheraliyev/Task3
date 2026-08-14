fff78d53474df5042c2058c0b85e2a91dc9249ea
Yodgorbek Sheraliyev <yodgorbeksheraliyev777@gmail.com>
2026-08-14 20:27:16 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+using System.Numerics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+var app = builder.Build();
+app.MapGet("/yodgorbeksheraliyev777@gmail.com", (string? x, string? y) =>
+{
+
+    if(!IsValidNumber(x) || !IsValidNumber(y))
+        return "NaN";
+    BigInteger a = BigInteger.Parse(x!);
+    BigInteger b = BigInteger.Parse(y!);
+
+    BigInteger gcd = BigInteger.GreatestCommonDivisor(a, b);
+    BigInteger lcm = (a * b ) / gcd ;
+
+    return lcm.ToString();
+
+});
+
+app.Run();
+
+static bool IsValidNumber(string value)
+{
+    if (string.IsNullOrEmpty(value))
+        return false;
+
+    foreach (char c in value)
+    {
+        if (c < '0' || c > '9')
+            return false;
+    }
+
+    return BigInteger.TryParse(value, out var n) && n > 0;
+}
